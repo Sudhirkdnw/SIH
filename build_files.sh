95630702c5e4824c9f71ae6602ecb8b9313821c7
Sudhir Kumar <92103626+Sudhirkdnw@users.noreply.github.com>
2024-09-13 10:32:17 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,5 @@
-echo "Building Django project"
+echo "BUILD START"
 python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput
+python3.9 manage.py collectstatic --noinput --clear
+
+echo "BUILD END"
